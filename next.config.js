e46c806e4f5066205d3597004d14af73604d54e3
AlexNotusDev <alexnotusdev@gmail.com>
2023-07-20 20:05:21 +0300

    Revert "update env var for amplify"
    This reverts commit f6b7c5a63ec1c3ac80ebc341c02c6f855f734f06.

--- next.config.js
+++ next.config.js
@@ -2,14 +2,11 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    apiUrl: `${process.env.HOST}/api`,
+    apiUrl: process.env.HOST + '/api',
     awsRegion: process.env.REGION_AWS,
     awsSecretAccessKey: process.env.SECRET_ACCESS_KEY_AWS,
     awsAccessKey: process.env.ACCESS_KEY_AWS,
     awsBucketName: process.env.BUCKET_NAME_AWS,
-    nextAuthSecret: process.env.NEXTAUTH_SECRET,
-    googleClientId: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   },
   experimental: {
     serverActions: true,
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,8 +7,8 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.googleClientId || '',
-      clientSecret: process.env.googleClientSecret || '',
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
   ],
   pages: {
@@ -18,7 +18,7 @@ export const authOptions: AuthOptions = {
   session: {
     strategy: 'jwt',
   },
-  secret: process.env.nextAuthSecret || '',
+  secret: process.env.NEXTAUTH_SECRET || '',
 };
 
 export default NextAuth(authOptions);
